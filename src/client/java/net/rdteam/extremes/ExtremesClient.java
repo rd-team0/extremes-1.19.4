7a89fc5127a0888ee64e54051735b816724e1484
Rdteam1397! <rdteam9999@gmail.com>
2024-04-28 22:44:09 +0300

    Add repository

---
+++ src/client/java/net/rdteam/extremes/ExtremesClient.java
@@ -0,0 +1,10 @@
+package net.rdteam.extremes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtremesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
